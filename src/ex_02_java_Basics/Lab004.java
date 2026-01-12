ea29e9cb9c8c2aebd68bd57531742f73913f0371
Kulshrest Parjapati <prjapatikk0@gmail.com>
2026-01-12 09:58:27 +0530

    12 jan add

---
+++ src/ex_02_java_Basics/Lab004.java
@@ -0,0 +1,8 @@
+package ex_02_java_Basics;
+
+public class Lab004 {
+    static void main(String[] args) {
+        System.out.println(2*2);
+        System.out.println(10/5);
+    }
+}
---
+++ src/ex_02_java_Basics/Lab006_main_method.java
@@ -0,0 +1,14 @@
+package ex_02_java_Basics;
+
+public class Lab006_main_method {
+    public static void main(String[] args) {
+        System.out.println("kk");
+    }
+
+   public static void main(String args) {
+       System.out.println("kk");
+    }
+    public static void Main(String [] args){
+        System.out.println("kk");
+    }
+}
