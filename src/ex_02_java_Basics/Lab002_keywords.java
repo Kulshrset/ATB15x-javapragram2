bf025a41baa8a48e2096c55f0d09e1bbc6e73510
Kulshrest Parjapati <prjapatikk0@gmail.com>
2026-01-12 09:44:53 +0530

    Initial commit

---
+++ src/ex_02_java_Basics/Lab002_keywords.java
@@ -0,0 +1,9 @@
+package ex_02_java_Basics;
+
+public class Lab002_keywords {
+    static void main(String[] args) {
+        System.out.println("keywords");
+        //package, public,class,static ,void
+
+    }
+}
---
+++ src/ex_02_java_Basics/Lab003_keyword.java
@@ -0,0 +1,13 @@
+package ex_02_java_Basics;
+
+public class Lab003_keyword {
+    static void main(String[] args) {
+        System.out.println("keywords");
+        //int a=10
+        //supers
+
+
+        //68 keywords presents in jdk24
+    }
+
+}
